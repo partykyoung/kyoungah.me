f0dd356d5581fa3f11a1d945914aff2ec6cf7db6
partykyoung <partykyoung@gmail.com>
2025-01-04 18:05:56 +0900

    feat: rollup, babel 사용해서 ui 라이브러리 환경 설정 완료

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,36 +1,71 @@
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { readdirSync, lstatSync } from "fs";
+import path from "path";
+
 import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+
 import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+import postcss from "rollup-plugin-postcss";
 
 import autoprefixer from "autoprefixer";
 
-export default {
-  input: {
-    button: "src/button/index.tsx",
-  },
+const components = readdirSync("./src")
+  .filter((dir) => {
+    const fullPath = path.resolve("./src", dir);
 
+    return (
+      lstatSync(fullPath).isDirectory() &&
+      readdirSync(fullPath).includes("index.tsx")
+    );
+  })
+  .reduce((acc, dir) => {
+    acc[dir] = `./src/${dir}/index.tsx`;
+    return acc;
+  }, {});
+
+export default {
+  input: components,
   output: [
     {
       dir: "build",
       format: "esm",
       entryFileNames: "[name]/index.js", // 컴포넌트별 폴더 생성
       sourcemap: false,
-      globals: { react: "React" },
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+        "react/jsx-runtime": "jsxRuntime",
+        clsx: "clsx",
+      },
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      exclude: "node_modules/**",
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: "> 0.25%, not dead",
+            modules: false,
+          },
+        ],
+        "@babel/preset-react",
+        "@babel/preset-typescript", // TypeScript 트랜스파일 지원
+      ],
+    }),
     commonjs(),
-    typescript(),
     postcss({
       plugins: [autoprefixer()],
       extract: false,
       modules: true,
-      extensions: [".scss", ".css"],
     }),
   ],
-  external: ["react", "react-dom", "clsx"],
+  external: ["react", "react-dom", "react/jsx-runtime", "clsx"],
 };
---
+++ packages/ui/src/input/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./input.module.css";
+
+function Input() {
+  return <div className={styles.root} />;
+}
+
+export { Input };
