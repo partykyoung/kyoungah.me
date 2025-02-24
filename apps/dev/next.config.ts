72d1a8e1c1907a97d74ea1cd2124d05b67aeaa1c
partykyoung <partykyoung@gmail.com>
2025-02-25 01:40:06 +0900

    feat: 마크다운 관련 dependency 설치

--- apps/dev/next.config.ts
+++ apps/dev/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["md", "ts", "tsx"],
 };
 
-export default withVanillaExtract(nextConfig);
+export default withVanillaExtract(withMDX(nextConfig));
