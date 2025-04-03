9d8009a65fed3aa5669d9bad9ab18687276563f8
partykyoung <partykyoung@gmail.com>
2025-04-04 04:50:05 +0900

    feat: enhance post detail components with ClientImage and improved styles

--- apps/dev/src/pages/post-detail/ui/post-detail-elements/post-detail-elements.css.ts
+++ apps/dev/src/pages/post-detail/ui/post-detail-elements/post-detail-elements.css.ts
@@ -3,6 +3,7 @@ import { style } from "@vanilla-extract/css";
 export const heading = style({
   marginTop: "2rem",
   marginBottom: "0.75rem",
+  fontWeight: 600,
 
   selectors: {
     ":is(h1, h2, h3, h4, h5, h6) + &": {
@@ -26,26 +27,59 @@ export const heading = style({
 
 export const list = style({
   paddingLeft: "1.625em",
-});
-
-export const li = style({
-  marginBottom: "1em", // Adjusted for spacing
-  paddingLeft: "0.25em", // Adjusted for alignment
-  fontWeight: "var(--font-weight-light)",
+  marginTop: "1em",
+  marginBottom: "1em",
 
   selectors: {
-    "li > ul > &": {
-      marginTop: "0.75em", // Adjusted for nested lists
+    "li  &": {
+      marginTop: "0",
+      marginBottom: "0",
     },
   },
 });
 
-export const p = style({
+export const li = style({
+  marginTop: "0.5em",
+  marginBottom: "0.75em",
+  paddingLeft: "0.25em",
   fontWeight: "var(--font-weight-light)",
 });
 
-export const img = style({});
+export const p = style({
+  marginTop: "1.25em",
+  marginBottom: "1.25em",
+  fontWeight: "var(--font-weight-light)",
 
-export const link = style({});
+  selectors: {
+    "li  &": {
+      marginTop: "0.5em",
+      marginBottom: "0.75em",
+    },
+  },
+});
+
+export const img = style({
+  margin: "3rem auto",
+  maxWidth: "100%",
+  height: "auto",
+});
+
+export const link = style({
+  fontWeight: "var(--font-weight-regular)",
+  color: "var(--primary)",
+
+  selectors: {
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+});
 
 export const pre = style({});
+
+export const code = style({
+  padding: "0.2em 0.4em",
+  backgroundColor: "rgba(var(--grey100-rgb), 0.4)",
+  borderRadius: "3px",
+  fontSize: "85%",
+});
