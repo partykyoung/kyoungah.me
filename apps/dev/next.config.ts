2be7134fa8648586a35f97809d017d5bf2d09b9e
partykyoung <partykyoung@gmail.com>
2025-04-04 15:09:18 +0900

    feat: simplify Next.js configuration and enhance post detail rendering with rehype plugins

--- apps/dev/next.config.ts
+++ apps/dev/next.config.ts
@@ -1,35 +1,9 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
-import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
-import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
-import rehypeExpressiveCode from "rehype-expressive-code";
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const rehypeExpressiveCodeOptions = {
-  themes: ["min-dark", "min-light"],
-  plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
-};
-
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [[rehypeExpressiveCode, rehypeExpressiveCodeOptions]],
-  },
-});
-
 const nextConfig: NextConfig = {
-  images: {
-    localPatterns: [
-      {
-        pathname: "/images/**",
-        search: "",
-      },
-    ],
-  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
@@ -38,4 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withVanillaExtract(withMDX(nextConfig));
+export default withVanillaExtract(nextConfig);
