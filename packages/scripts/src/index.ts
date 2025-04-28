59ac495e3fb585e096b68302c9643da2e69e9099
partykyoung <partykyoung@gmail.com>
2025-04-28 20:05:57 +0900

    feat: JSON 파일 및 스크립트 추가
    - package.json에서 스크립트 수정
    - 새로운 JSON 파일(aws1.json, etc1.json, gatsby1.json, html1.json, next.js1.json, react1.json, redux1.json, 상태관리1.json) 추가
    - create-tag-json.ts 파일 추가 및 태그 JSON 생성 로직 구현
    - index.ts에서 createTagJson 함수 호출 추가

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,5 +1,7 @@
 import { createPaginationJson } from "./create-pagination-json.js";
 import { createTagsJson } from "./create-tags-json.js";
+import { createTagJson } from "./create-tag-json.js";
 
 createPaginationJson();
 createTagsJson();
+createTagJson();
