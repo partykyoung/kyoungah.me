f74bc9f9d24f1ff447c4dec00e86f9b24942021a
partykyoung <partykyoung@gmail.com>
2025-04-28 19:27:16 +0900

    feat: JSON 파일 및 스크립트 수정
    - page1.json 및 page2.json의 포스트 데이터 수정
    - tags.json 형식 변경: 태그 카운트를 문자열로 변환
    - create-tags-json.ts에서 태그 집계 로직 개선
    - index.ts에서 명령어 관련 코드 제거 및 직접 호출로 변경
    - tsconfig.json에서 모듈 설정을 NodeNext로 변경

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,24 +1,5 @@
-import { Command } from "commander";
+import { createPaginationJson } from "./create-pagination-json.js";
+import { createTagsJson } from "./create-tags-json.js";
 
-import { createPaginationJson } from "./create-pagination-json";
-import { createTagsJson } from "./create-tags-json";
-
-const program = new Command();
-
-program
-  .name("scripts")
-  .command("gen:pagination")
-  .description("Generate pagination JSON")
-  .action(() => {
-    createPaginationJson();
-  });
-
-program
-  .name("scripts")
-  .command("gen:tags")
-  .description("Generate tags JSON")
-  .action(() => {
-    createTagsJson();
-  });
-
-program.parse(process.argv);
+createPaginationJson();
+createTagsJson();
