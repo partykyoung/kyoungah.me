b2a1bf7e8bd98202e9c338868b4ba36379aab41f
partykyoung <partykyoung@gmail.com>
2025-05-05 16:04:29 +0900

    feat: 블로그 포스트 생성 스크립트 추가
    - 새로운 포스트 생성을 위한 create-new-post.ts 파일 추가
    - package.json에 gen:post 스크립트 추가
    - 스크립트에서 사용될 @inquirer/checkbox, @inquirer/input, dayjs 의존성 추가
    - script.config.mjs에 gen:post 설정 추가
    - tsconfig.tsbuildinfo에 create-new-post.ts 경로 추가

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,7 +1,40 @@
+import { Command } from "commander";
+import { createNewPost } from "./create-new-post.js";
 import { createPaginationJson } from "./create-pagination-json.js";
-import { createTagsJson } from "./create-tags-json.js";
 import { createTagJson } from "./create-tag-json.js";
+import { createTagsJson } from "./create-tags-json.js";
 
-createPaginationJson();
-createTagsJson();
-createTagJson();
+const program = new Command();
+
+program.name("scripts").description("Blog management scripts").version("1.0.0");
+
+program
+  .command("gen:post")
+  .description("Generate a new blog post")
+  .action(async () => {
+    await createNewPost();
+  });
+
+program
+  .command("build:jsons")
+  .description("Build all JSON files")
+  .action(async () => {
+    await Promise.all([
+      createPaginationJson(),
+      createTagJson(),
+      createTagsJson(),
+    ]);
+  });
+
+// Default command when no arguments provided
+if (!process.argv.slice(2).length) {
+  program.action(async () => {
+    await Promise.all([
+      createPaginationJson(),
+      createTagJson(),
+      createTagsJson(),
+    ]);
+  });
+}
+
+program.parse();
