82d9ab647d0f25235188e75dc74d0e071d5525f8
partykyoung <partykyoung@gmail.com>
2025-05-28 13:48:23 +0900

    feat: QueryClient 및 관련 모듈 추가
    - QueryClient를 core 모듈에 추가
    - index.ts에서 QueryClient를 export하도록 수정
    - tsup.config.ts에서 minify 및 shims 설정 제거
    - react/index.ts 파일 생성 및 useQueryClient, QueryClientContextProvider export

---
+++ packages/query/src/core/index.ts
@@ -0,0 +1,6 @@
+import { Query } from "./query.js";
+import { QueryCache } from "./query-cache.js";
+import { QueryClient } from "./query-client.js";
+import { QueryObserver } from "./query-observer.js";
+
+export { Query, QueryCache, QueryClient, QueryObserver };
---
+++ packages/query/src/react/index.ts
@@ -0,0 +1,7 @@
+import { useQuery } from "./use-query.js";
+import {
+  useQueryClient,
+  QueryClientContextProvider,
+} from "./query-client-context.jsx";
+
+export { useQuery, useQueryClient, QueryClientContextProvider };
