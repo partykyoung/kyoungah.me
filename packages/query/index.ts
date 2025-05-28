01b715cb450ea49c72ed2a4a7bca62c8f4a3b9af
partykyoung <partykyoung@gmail.com>
2025-05-28 13:37:57 +0900

    feat(query): update package.json and add tsup configuration
    - Set up bin entry for the build script in package.json.
    - Add development and build scripts using tsup.
    - Introduce tsup configuration file for bundling TypeScript files.
    - Include necessary dependencies for tsup in package.json and pnpm-lock.yaml.

---
+++ packages/query/index.ts
@@ -0,0 +1,7 @@
+import { useQuery } from "./src/react/use-query.js";
+import {
+  useQueryClient,
+  QueryClientContextProvider,
+} from "./src/react/query-client-context.jsx";
+
+export { useQuery, useQueryClient, QueryClientContextProvider };
---
+++ packages/query/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup";
+import { resolve } from "path";
+
+export default defineConfig({
+  entry: {
+    index: resolve(__dirname, "index.ts"), // 진입점
+  },
+  format: ["esm"], // ESModule 형식으로 번들
+  dts: true, // TypeScript 타입 정의 파일 생성
+  clean: true, // build 전에 outDir 비우기
+  outDir: "build", // 출력 디렉토리
+  external: ["react", "react-dom", "react/jsx-runtime"], // 번들에서 제외할 외부 모듈
+
+  esbuildOptions(options) {
+    options.jsx = "automatic"; // React 17+ JSX 방식 사용
+    options.platform = "neutral"; // node/browser 둘 다 호환
+    options.target = ["es2020"]; // 지원 타겟 (모던 브라우저 기준)
+  },
+  treeshake: true, // 사용되지 않는 코드 제거
+  minify: false, // 디버깅을 위해 압축 비활성화
+  shims: true, // 일부 node 기능 shim
+  onSuccess: "echo 'Build completed successfully!'", // 빌드 성공 시 메시지 출력
+});
