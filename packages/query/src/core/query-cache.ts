288361d97b8b976c95ae6eb3316f7498a24c9ec0
partykyoung <partykyoung@gmail.com>
2025-05-28 10:13:29 +0900

    feat: QueryCache 클래스 및 QueryStore 인터페이스 추가
    - QueryCache 클래스의 기본 구조를 정의하고, 쿼리 캐시 기능을 구현함.
    - QueryStore 인터페이스를 추가하여 쿼리 관리 메서드를 정의함.
    - build 메서드를 구현하여 쿼리 생성 및 캐싱 로직을 추가함.
    - querys 맵을 사용하여 쿼리 상태를 관리함.

--- packages/query/src/core/query-cache.ts
+++ packages/query/src/core/query-cache.ts
@@ -1,3 +1,50 @@
-class QueryCache {}
+import { QueryClient } from "./query-client.js";
+import { Query } from "./query.js";
+import { hashQueryKeyByOptions } from "./utils.js";
+
+export interface QueryStore {
+  has: (queryHash: string) => boolean;
+  set: (queryHash: string, query: Query) => void;
+  get: (queryHash: string) => Query | undefined;
+  delete: (queryHash: string) => void;
+  values: () => IterableIterator<Query>;
+}
+
+class QueryCache {
+  private querys: Map<string, Query>;
+
+  constructor() {
+    this.querys = new Map<string, Query>();
+  }
+
+  build(client: QueryClient, options: any) {
+    const queryKey = options.queryKey;
+    const queryHash =
+      options.queryHash ?? hashQueryKeyByOptions(queryKey, options);
+    let query = this.get(queryHash);
+
+    if (!query) {
+      query = new Query({
+        client,
+        queryKey,
+        queryHash,
+        options: client.defaultQueryOptions(options),
+        state,
+        defaultOptions: client.getQueryDefaults(queryKey),
+      });
+      this.add(query);
+    }
+
+    return query;
+  }
+
+  get() {}
+
+  getAll() {}
+
+  add() {}
+
+  notify() {}
+}
 
 export { QueryCache };
