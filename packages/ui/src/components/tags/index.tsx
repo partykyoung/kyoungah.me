e4b67a1884d873892e50616bffb54f53eba6c9ea
partykyoung <partykyoung@gmail.com>
2025-05-30 14:05:38 +0900

    feat: Figma API 키 입력 방식 개선 및 태그 컴포넌트 수정
    - Figma API 키 입력 방식에서 id를 'figmaAPIKey'로 변경
    - 태그 컴포넌트에 크기(size) 속성 추가
    - 태그 컴포넌트의 클래스 이름에 따라 스타일 적용
    - CSS 스타일을 개선하여 태그의 레이아웃 및 디자인 조정

--- packages/ui/src/components/tags/index.tsx
+++ packages/ui/src/components/tags/index.tsx
@@ -8,13 +8,27 @@ import styles from "./tags.module.css";
 
 interface TagsProps extends React.HTMLAttributes<HTMLDivElement> {
   asChild?: boolean;
+  size?: "sm" | "md";
 }
 
-function Tags({ asChild, children, className, ...props }: TagsProps) {
+function Tags({
+  asChild,
+  children,
+  className,
+  size = "md",
+  ...props
+}: TagsProps) {
   const Comp = asChild ? Slot : "div";
 
   return (
-    <Comp className={clsx(styles.tags, className)} {...props}>
+    <Comp
+      className={clsx(
+        styles.tags,
+        size === "sm" ? styles["tags-sm"] : styles["tags-md"],
+        className
+      )}
+      {...props}
+    >
       {children}
     </Comp>
   );
@@ -23,13 +37,27 @@ function Tags({ asChild, children, className, ...props }: TagsProps) {
 // ----------- Tag ----------- //
 interface TagProps extends React.HTMLAttributes<HTMLSpanElement> {
   asChild?: boolean;
+  size?: "sm" | "md";
 }
 
-function Tag({ asChild, children, className, ...props }: TagProps) {
+function Tag({
+  asChild,
+  children,
+  className,
+  size = "md",
+  ...props
+}: TagProps) {
   const Comp = asChild ? Slot : "span";
 
   return (
-    <Comp className={clsx(styles.tag)} {...props}>
+    <Comp
+      className={clsx(
+        styles.tag,
+        size === "sm" ? styles["tag-sm"] : styles["tag-md"],
+        className
+      )}
+      {...props}
+    >
       {children}
     </Comp>
   );
